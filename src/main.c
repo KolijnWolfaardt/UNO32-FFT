c5b47eb1e661e70caafd532f962ce257d9501a04
Kolijn Wolfaardt <kolijn.wolfaardt@gmail.com>
2013-06-29 11:02:54 +0200

    Added 2 tests

--- src/main.c
+++ src/main.c
@@ -7,18 +7,28 @@
 int main(void)
 {
 
-	double data8A[] = {1,2,3,2,1,0,-1,-2};
-	double data8B[] = {0,0,0,0,0,0,0,0};
-	int N = 8;
+	//Some test cases:
+	double data16A[] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	double data16B[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+	double data32A[] = {1,0.938,0.324,-0.057,0.293,1.119,1.631,1.214,0,-1.214,-1.631,-1.119,-0.293,0.057,-0.324,-0.938,-1,-0.173,1.09,1.905,1.707,0.729,-0.217,-0.449,0,0.449,0.217,-0.729,-1.707,-1.905,-1.09,0.173};
+	double data32B[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
 	int loop = 0;
 
+	fft(data16A,data16B,16);
 
-	fft(data8A,data8B,N);
-
-	printf("\n");
-	for (loop=0; loop<N;loop++)
+	printf("\nDoing FFT with an impulse. Output should be a just 1s");
+	for (loop=0; loop<16;loop++)
 	{
-		printf("%d\t%f+i%f\n",loop,data8A[loop],data8B[loop]);
+		printf("%d\t%f+i%f\n",loop,data16A[loop],data16B[loop]);
+	}
+
+	fft(data32A,data32B,32);
+	printf("\nDoing FFT with an a sine and a cosine.Cosine at 5, and sine at 2");
+	for (loop=0; loop<32;loop++)
+	{
+		printf("%d\t%f+i%f\n",loop,data32A[loop],data32B[loop]);
 	}
 
 	return 0;
