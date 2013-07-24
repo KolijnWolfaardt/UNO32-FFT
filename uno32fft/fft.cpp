5004c3eef1fed46f1444bed198fe6460ac7c54d6
Kolijn Wolfaardt <kolijn.wolfaardt@gmail.com>
2013-07-24 23:13:46 +0200

    Fixed spelling error

--- uno32fft/fft.cpp
+++ uno32fft/fft.cpp
@@ -1,7 +1,7 @@
 /*
 This file is release under the MIT licence.
 
-This is an implementation of the Cooley-Turkey FFT algorithm. It is supposed
+This is an implementation of the Cooley-Tukey FFT algorithm. It is supposed
 to be used on a microcontroller, so it will be optimized for low-memory, no
 floating point numbers and no math library.
 */
